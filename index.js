db3a77d4ac4d6c699501186d1ee14da2798b9f57
yusfifarkhan <93718536+yusfifarkhan@users.noreply.github.com>
2022-06-03 21:41:17 +0700

    Submit #1
    Membuat fungsi sorting array nama secara ascending dan desending dan menambahkan penomoran.

--- index.js
+++ index.js
@@ -6,15 +6,31 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (arrayNames, cbSorting) => {
+  const sortedArray = cbSorting(arrayNames);
+  const finalArray = (arrayToFormat) => {
+    const formatedArray = [];
+    for (let index = 0; index < arrayToFormat.length; index++) {
+      formatedArray.push(`${index+1}. ${arrayToFormat[index]}`);
+    }
+    return formatedArray;
+  }
+  return finalArray(sortedArray);
+};
+
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (arrayNames) => {
+  return arrayNames.sort()
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (arrayNames) => {
+  return arrayNames.sort().reverse()
+};
+
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
